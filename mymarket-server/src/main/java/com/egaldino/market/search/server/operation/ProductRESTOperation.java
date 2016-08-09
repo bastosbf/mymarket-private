c6a0b380551b03a68a65a6cb462c3835ba0a487b
egaldino <edson1galdino@gmail.com>
2016-08-09 14:18:58 -0300

    Retirando list-by-<market/product>

--- mymarket-server/src/main/java/com/egaldino/market/search/server/operation/ProductRESTOperation.java
+++ mymarket-server/src/main/java/com/egaldino/market/search/server/operation/ProductRESTOperation.java
@@ -1,7 +1,5 @@
 package com.egaldino.market.search.server.operation;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,29 +7,12 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.egaldino.market.search.server.HibernateConfig;
-import com.egaldino.market.search.server.dao.MarketProductDAO;
 import com.egaldino.market.search.server.dao.ProductDAO;
 import com.egaldino.market.search.server.model.Product;
 
 @Path("/product")
 public class ProductRESTOperation {
-
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	@Path("/list-by-market")
-	public List<Product> listByMarket(@QueryParam("market") int market) {
-		ProductDAO dao = new ProductDAO(HibernateConfig.factory);
-		return dao.listByMarket(market);
-	}
-
-	@GET
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	@Path("/list-by-place")
-	public List<Product> listByPlace(@QueryParam("place") int place) {
-		ProductDAO dao = new ProductDAO(HibernateConfig.factory);
-		return dao.listByPlace(place);
-	}
-
+	
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Path("/get")
